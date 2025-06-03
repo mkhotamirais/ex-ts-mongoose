a7363963d10d41f52fb13bd4e6d5cce98d33db2b
mkhotamirais <mkhotamirais@gmail.com>
2025-06-03 15:29:32 +0700

    update origins

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,1 +1,5 @@
-export const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+export const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "https://next-mern-mkhotami.vercel.app",
+];
