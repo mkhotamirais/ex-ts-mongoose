2f609f24d87955174757fd19c0912560a4602591
mkhotamirais <mkhotamirais@gmail.com>
2025-06-23 20:19:35 +0700

    selesai juga

--- src/app/router.search.ts
+++ src/app/router.search.ts
@@ -10,11 +10,14 @@ router.get("/search", async (req: Request, res: Response) => {
     const search = typeof req.query.search === "string" ? req.query.search : "";
 
     const posts = await Posts.find({ title: { $regex: search, $options: "i" } })
+      .sort("title")
       .select(["title", "imageUrl"])
       .lean();
+
     const products = await Products.find({
       $or: [{ name: { $regex: search, $options: "i" } }, { description: { $regex: search, $options: "i" } }],
     })
+      .sort("price")
       .select(["name", "price", "imageUrl"])
       .lean();
 
